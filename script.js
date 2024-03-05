3b934fc9cf7746d5356932d7e07464f6538d04b6
Abir Chodha <abirchodha1@gmail.com>
2024-03-06 00:58:37 +0530

    Close modal, overlay on clicking cross

--- script.js
+++ script.js
@@ -11,3 +11,8 @@ for (let i = 0; i < btnOpenModal.length; i++) {
     overlay.classList.remove('hidden');
   });
 }
+
+btnCloseModal.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
