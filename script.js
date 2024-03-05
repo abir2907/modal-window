aa7f0b1a0ff740fa0f6bdb81a0b7d5be91658add
Abir Chodha <abirchodha1@gmail.com>
2024-03-06 00:52:51 +0530

    Remove hidden class on clicking show modal

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+  });
+}
